dee96db6cfb5fa9ca347b896b63bca1f2bde3a1c
Ruben Dario Suarez Diaz <ruben.suarezdz@udlap.mx>
2024-04-07 17:20:15 -0600

    NUEVO

--- Form1.cs
+++ Form1.cs
@@ -10,14 +10,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
-namespace ExamenParcial
+ 
+namespace BASICFORM
 {
     public partial class Form1 : Form
     {
         private Bitmap bmp;
-        private Scene scene;
-        private Canvas canvas;
 
         public Form1()
         {
@@ -33,10 +31,6 @@ namespace ExamenParcial
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-            string pathToObj = Path.Combine(Application.StartupPath, "MYOBJ.obj");
-            ObjLoader objLoader = new ObjLoader(pathToObj);
-            scene = new Scene(objLoader);
-
             TIMER.Interval = 16;
             TIMER.Start();
         }
@@ -53,7 +47,7 @@ namespace ExamenParcial
             {
                 // Asigna el color base o gestiona la lógica para cambiarlo según sea necesario.
                 Color colorBase = Color.FromArgb(0, 182, 0);
-                scene.Projection(graphics, PCT_CANVAS, colorBase); 
+           
             }
 
             // Actualiza la imagen del PictureBox con el nuevo bitmap.
@@ -62,50 +56,36 @@ namespace ExamenParcial
 
         private void TIMER_Tick(object sender, EventArgs e)
         {
-            scene.Update(); // Actualiza la escena, si es necesario.
-            RenderScene(); // Llama al nuevo método de renderizado.
+       
         }
 
 
         private void CHBX_ROTX_CheckedChanged(object sender, EventArgs e)
         {
-            scene.RotateXEnabled = CHBX_ROTX.Checked;
-            RenderScene();
+         
         }
 
         private void CHBX_ROTY_CheckedChanged(object sender, EventArgs e)
         {
-            scene.RotateYEnabled = CHBX_ROTY.Checked;
-            RenderScene();
+      
         }
 
         private void CHBX_ROTZ_CheckedChanged(object sender, EventArgs e)
         {
 
-            scene.RotateZEnabled = CHBX_ROTZ.Checked;
-            RenderScene();
+     
         }
 
 
 
         private void BTN_1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Antes de la traslación:");
-            scene.PrintVerticesPositions();
 
-            // Aplicar una traslación más pequeña
-            scene.TranslateAllMeshes(1, 0.5f, 0);
-
-            Console.WriteLine("Después de la traslación:");
-            scene.PrintVerticesPositions();
         }
 
         private void BTN_ESCALAR_Click(object sender, EventArgs e)
         {
-                // Aplica un factor de escala a toda la escena
-                scene.ScaleAllMeshes(0.9f, 0.9f, 0.9f); // Esto escalará todos los objetos en la escena un 10% más grandes
-
-                RenderScene(); // Asegúrate de volver a renderizar la escena para ver los cambios
+            
 
         }
 
--- Program.cs
+++ Program.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ExamenParcial
+namespace BASICFORM
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
